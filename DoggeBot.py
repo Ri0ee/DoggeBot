e7b4e91ee77a683c56eb213a796a8ac8ff10cfb6
Kirill <kirilltrofimov@inbox.lv>
2019-04-26 00:03:57 +0300

    Fixed key loading, enhanced code a bit

--- DoggeBot.py
+++ DoggeBot.py
@@ -1,34 +1,41 @@
 from telegram.ext import Updater, CommandHandler
 import requests
-import re
+
 
 def get_dogge_url():
     contents = requests.get('https://random.dog/woof.json').json()
     image_url = contents['url']
     return image_url
 
+
 def show_dogge(bot, update):
     print('Showing Dogge')
     url = get_dogge_url()
     chat_id = update.message.chat_id
-    bot.send_photo(chat_id = chat_id, photo = url)
+    bot.send_photo(chat_id=chat_id, photo=url)
+
 
 def show_human(bot, update):
     print('Showing Human')
     chat_id = update.message.chat_id
-    bot.send_photo(chat_id = chat_id, photo = 'https://thispersondoesnotexist.com/image')
+    bot.send_photo(chat_id=chat_id, photo='https://thispersondoesnotexist.com/image')
+
 
 def main():
     f = open("key.txt", "r")
-    key = f.readline()
+    key = f.read()[:-1]
     f.close()
 
-    updater = Updater(key)
+    updater = Updater(token=key)
     dp = updater.dispatcher
+
     dp.add_handler(CommandHandler('dogge', show_dogge))
     dp.add_handler(CommandHandler('human', show_human))
+
+    print("Starting polling...")
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
